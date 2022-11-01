a4c544df798178fb04562538551677ed045a725e
Kenneth Wang <109991402+kennlw@users.noreply.github.com>
2022-11-01 14:55:58 -0400

    Drivetrain Prototype

---
+++ src/main/java/frc/robot/commands/DriveArcade.java
@@ -0,0 +1,50 @@
+package frc.robot.commands;
+import frc.robot.subsystems.Drivetrain;
+import java.util.function.Supplier;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class DriveArcade extends CommandBase {
+  //creates driveSubsystem object from the DriveSubsystem class/file
+  private final Drivetrain drivetrain;
+  //creates a lamda expression that lets us grab the true value of objects and not a just static value
+  private Supplier<Double> speedLeft, speedRight, speed;
+
+  public DriveArcade(Drivetrain drive, Supplier<Double> L, Supplier<Double> R, Supplier<Double> S){
+    this.drivetrain = drive;
+    this.speedLeft = L;
+    this.speedRight = R;
+    this.speed = S;
+
+    addRequirements(drivetrain);
+  }
+  
+  // Called just before this Command runs the first time
+  @Override
+  public void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  public void execute() {
+    //Gets speeds of the left and right sides plus the overall speed
+    drivetrain.setMotors(speedLeft.get(), speedRight.get() * -1, speed.get());
+    
+    //sends the data to the smart dash which can be seen on the driver station
+    SmartDashboard.putNumber("Left side speed",speedLeft.get());
+    SmartDashboard.putNumber("Right side speed",speedRight.get());
+    SmartDashboard.putNumber("speed",speed.get());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import frc.robot.Constants;
+
+/**
+ * Add your docs here.
+ */
+public class Drivetrain extends SubsystemBase {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  public WPI_VictorSPX leftPrimary = new WPI_VictorSPX(Constants.DRIVETRAIN_LEFT_FRONT_MOTOR);
+  public WPI_VictorSPX leftSecondary = new WPI_VictorSPX(Constants.DRIVETRAIN_LEFT_BACK_MOTOR);
+  public WPI_VictorSPX rightPrimary = new WPI_VictorSPX(Constants.DRIVETRAIN_RIGHT_FRONT_MOTOR);
+  public WPI_VictorSPX rightSecondary = new WPI_VictorSPX(Constants.DRIVETRAIN_RIGHT_BACK_MOTOR);
+
+  MotorControllerGroup leftMotors = null;
+  MotorControllerGroup rightMotors = null;
+
+  DifferentialDrive differentialDrive = null;
+
+  public Drivetrain() {
+    leftMotors = new MotorControllerGroup(leftPrimary, leftSecondary);
+    rightMotors = new MotorControllerGroup(rightPrimary, rightSecondary);
+
+    differentialDrive = new DifferentialDrive(leftMotors, rightMotors);
+  }
+
+  public void arcadeDrive(double moveSpeed, double rotateSpeed) {
+    differentialDrive.arcadeDrive(moveSpeed, rotateSpeed);
+  }
+
+  public void DriveInversion(){
+    leftMotors.setInverted(true);
+    rightMotors.setInverted(true);
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+  public void setMotors(double leftSpeed, double rightSpeed, double Speed) {
+    differentialDrive.arcadeDrive(leftSpeed * Speed, rightSpeed * Speed);
+  }
+}
