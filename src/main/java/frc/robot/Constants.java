15f6bbaba71749c6b0ddcf943ad00cf0b291102c
davidcaptilist <117213331+davidcaptilist@users.noreply.github.com>
2022-12-08 14:58:42 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,32 +6,28 @@ package frc.robot;
 
 public class Constants {
   // MOTORs
-  public static final int DRIVETRAIN_LEFT_FRONT_MOTOR = 22;
-  public static final int DRIVETRAIN_LEFT_BACK_MOTOR = 23;
-  public static final int DRIVETRAIN_RIGHT_FRONT_MOTOR = 20;
-  public static final int DRIVETRAIN_RIGHT_BACK_MOTOR = 21;
+//   public static final int DRIVETRAIN_LEFT_FRONT_MOTOR = 22;
+//   public static final int DRIVETRAIN_LEFT_BACK_MOTOR = 23;
+//   public static final int DRIVETRAIN_RIGHT_FRONT_MOTOR = 20;
+//   public static final int DRIVETRAIN_RIGHT_BACK_MOTOR = 21;
 
-  public static final int MANIPULATOR_ARM_MOTOR = 20;
-  public static final int MANIPULATOR_LIFT_MOTOR = 20;
+  
 
-  public static final Double speed = 1.0;
+  public static final Double speed = 0.6;
   // Joysticks
   public static final int JOYSTICK_PORT = 0;  
   public static final int DRIVER_CONTROLLER_1 = 0;
   public static final int DRIVER_CONTROLLER_2 = 0;
 
-  //Lifting motor
-  public static final int ELEVATION = 2;
-  public static final double ELEVATION_SPEED = 0.5;
-   
-  //Clamping motor
-  public static final int CLAMP = 0;
-  public static final double CLAMP_SPEED = 0.5;
+  //manipulator motors
+  public static final int MANIPULATOR_TURN_MOTOR= 22;
+  public static final int MANIPULATOR_LIFT_MOTOR = 20;
+  public static final double ELEVATION_SPEED = 0.6;
 
         
   //This tells us which part of the joystick will be used
   // 1 = to the left stick's y axis
-  public static final int RIGHT_AXIS = 4; 
+  public static final int RIGHT_AXIS = 5; 
   // 5 = to the right stick's y axis 
   public static final int LEFT_AXIS = 1;  
   // 1 = the a button on joystick
@@ -41,6 +37,6 @@ public class Constants {
   // 5 = the up on the d pad
   public static final int DAPD_UP = 5;
   // 6 = down on the d pad
-  public static final int DAPD_DOWN = 6;
+  public static final int DAPD_DOWN = 0;
 // Change for your controller
 }
